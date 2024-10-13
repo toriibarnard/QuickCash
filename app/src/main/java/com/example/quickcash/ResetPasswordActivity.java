ebd9ca4c0fb25f503de118a70d784d83716a9365
willyyyyc <willcarmichaelcf@gmail.com>
2024-10-13 19:18:51 -0300

    Resolved merge conflicts

--- app/src/main/java/com/example/quickcash/ResetPasswordActivity.java
+++ app/src/main/java/com/example/quickcash/ResetPasswordActivity.java
@@ -7,8 +7,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-public class PasswordReset extends AppCompatActivity {
+public class ResetPasswordActivity extends AppCompatActivity {
 
     private FirebasePasswordReset firebasePasswordReset;
 
@@ -16,7 +19,11 @@ public class PasswordReset extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset);  // Ensure the correct layout is used
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         firebasePasswordReset = new FirebasePasswordReset();
 
         this.setupPasswordResetButton();
@@ -32,16 +39,16 @@ public class PasswordReset extends AppCompatActivity {
                     firebasePasswordReset.sendPasswordResetEmail(email, new FirebasePasswordReset.ResetPasswordCallback() {
                         @Override
                         public void onSuccess() {
-                            Toast.makeText(PasswordReset.this, "Password reset email sent!", Toast.LENGTH_LONG).show();
+                            Toast.makeText(ResetPasswordActivity.this, "Password reset email sent!", Toast.LENGTH_LONG).show();
                         }
 
                         @Override
                         public void onFailure(Exception e) {
-                            Toast.makeText(PasswordReset.this, "Failed to send reset email: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email: " + e.getMessage(), Toast.LENGTH_LONG).show();
                         }
                     });
                 } else {
-                    Toast.makeText(PasswordReset.this, "Please enter your email address.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ResetPasswordActivity.this, "Please enter your email address.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
