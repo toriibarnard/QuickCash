dc65aff10aeda324d053f1c591e762f7a848ca89
Kayhan Talebzadeh <ky245805@dal.ca>
2024-10-18 11:57:31 -0300

    added JUnit and Espresso tests for the job application page

---
+++ app/src/test/java/com/example/quickcash/JobApplicationTest.java
@@ -0,0 +1,31 @@
+package com.example.quickcash;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JobApplicationTest {
+    RegistrationValidator validator;
+
+    @Before
+    public void setup() { validator = new RegistrationValidator(); }
+
+    @Test
+    public void checkIsValidEmail() {
+        assertTrue(validator.isValidEmail("abc123@gmail.com"));
+    }
+
+    @Test
+    public void checkIsValidPhone() {
+        assertTrue(validator.isValidPhone("5064295235"));
+    }
+
+    @Test
+    public void checkIsValidName() {
+        assertTrue(validator.isValidName("Kayhan"));
+    }
+
+    @Test
+    public void checkIsFileUploaded() {assertTrue(validator.isFileUploaded("resume.pdf"));}
+}
