5942b05dfec0dec58e7d204379d777ff767b376b
Furqaan Malik <fr250738@dal.ca>
2024-10-18 12:11:11 -0300

    Created and setup JobApplicationActivity, and updated some designs

--- app/src/main/java/com/example/quickcash/JobApplicationActivity.java
+++ app/src/main/java/com/example/quickcash/JobApplicationActivity.java
@@ -1,6 +1,8 @@
 package com.example.quickcash;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,40 @@ public class JobApplicationActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setUploadResumeButton();
+        setSubmitButton();
+    }
+
+    protected String getName() {
+        EditText name = findViewById(R.id.applicationNameBox);
+        return name.getText().toString().trim();
+    }
+
+    protected String getPhone() {
+        EditText phone = findViewById(R.id.applicationPhoneBox);
+        return phone.getText().toString().trim();
+    }
+
+    protected String getEmail() {
+        EditText email = findViewById(R.id.applicationEmailBox);
+        return email.getText().toString().trim();
+    }
+
+    public void setUploadResumeButton() {
+        Button uploadResume = findViewById(R.id.resumeUploadButton);
+        uploadResume.setOnClickListener(view -> handelUploadResume());
+    }
+
+    public void handelUploadResume() {
+
+    }
+
+    public void setSubmitButton() {
+        Button submitApplication = findViewById(R.id.applicationSubmitButton);
+        submitApplication.setOnClickListener(view -> handelSubmitApplication());
+    }
+
+    public void handelSubmitApplication() {
+
     }
 }
